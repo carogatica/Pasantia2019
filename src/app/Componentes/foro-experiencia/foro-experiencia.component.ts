44079b2e7a8c7fba32f7db7635614a0f1ac6b9f2
leo2749 <sposettileonelm@gmail.com>
2019-10-03 11:05:26 -0300

    hola manu

---
+++ src/app/Componentes/foro-experiencia/foro-experiencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foro-experiencia',
+  templateUrl: './foro-experiencia.component.html',
+  styleUrls: ['./foro-experiencia.component.css']
+})
+export class ForoExperienciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
