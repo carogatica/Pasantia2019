cf324749750994f0f72efb040a3a6ad1f444d75c
n1c0l454 <arenasnic@gmail.com>
2019-10-07 23:16:25 -0300

    routing 2.0 todos los enlaces funcionales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,17 @@ import { AppRoutingModule, routingcomoponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './Componentes/inicio/inicio.component';
-<<<<<<< HEAD
-import { ForoExperienciaComponent } from './Componentes/foro-experiencia/foro-experiencia.component';
-import { FormularioComponent } from './Componentes/formulario/formulario.component';
-=======
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
->>>>>>> nicolas
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TextFieldModule } from '@angular/cdk/text-field';
-<<<<<<< HEAD
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
-=======
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { NoticiasComponent } from './componentes/noticias/noticias.component';
-<<<<<<< HEAD
-
->>>>>>> nicolas
-
-=======
->>>>>>> nicolas
 @NgModule({
   declarations: [
     AppComponent,
